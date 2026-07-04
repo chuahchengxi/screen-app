25d1175878df9c9fd52c61fcb2fbd2a44a68cd83
chuahchengxi <boyboychuah@hotmail.com>
2026-07-04 10:05:13 +0800

    Initial Commit

---
+++ screen-app/screen_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  screen_appApp.swift
+//  screen-app
+//
+//  Created by cheng xi on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct screen_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
